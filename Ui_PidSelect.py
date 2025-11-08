3bc645d2c169d3cacb25befa218fa6794fd040dc
林洛铭 <992271363@qq.com>
2025-11-08 19:59:05 +0800

    面向ai编程这一块

--- Ui_PidSelect.py
+++ Ui_PidSelect.py
@@ -22,13 +22,13 @@ class Ui_KokoroJourney(object):
     def setupUi(self, KokoroJourney):
         if not KokoroJourney.objectName():
             KokoroJourney.setObjectName(u"KokoroJourney")
-        KokoroJourney.resize(277, 146)
+        KokoroJourney.resize(363, 170)
         self.pushButton_procs = QPushButton(KokoroJourney)
         self.pushButton_procs.setObjectName(u"pushButton_procs")
-        self.pushButton_procs.setGeometry(QRect(70, 100, 131, 31))
+        self.pushButton_procs.setGeometry(QRect(110, 120, 131, 31))
         self.layoutWidget = QWidget(KokoroJourney)
         self.layoutWidget.setObjectName(u"layoutWidget")
-        self.layoutWidget.setGeometry(QRect(90, 20, 181, 62))
+        self.layoutWidget.setGeometry(QRect(90, 20, 261, 84))
         self.verticalLayout = QVBoxLayout(self.layoutWidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
@@ -47,9 +47,14 @@ class Ui_KokoroJourney(object):
 
         self.verticalLayout.addWidget(self.proc_start_time_show)
 
+        self.proc_end_time_show = QLabel(self.layoutWidget)
+        self.proc_end_time_show.setObjectName(u"proc_end_time_show")
+
+        self.verticalLayout.addWidget(self.proc_end_time_show)
+
         self.layoutWidget1 = QWidget(KokoroJourney)
         self.layoutWidget1.setObjectName(u"layoutWidget1")
-        self.layoutWidget1.setGeometry(QRect(1, 20, 81, 62))
+        self.layoutWidget1.setGeometry(QRect(1, 20, 81, 84))
         self.verticalLayout_2 = QVBoxLayout(self.layoutWidget1)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
@@ -65,11 +70,17 @@ class Ui_KokoroJourney(object):
 
         self.verticalLayout_2.addWidget(self.proc_path_label)
 
-        self.label = QLabel(self.layoutWidget1)
-        self.label.setObjectName(u"label")
-        self.label.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.start_time_label = QLabel(self.layoutWidget1)
+        self.start_time_label.setObjectName(u"start_time_label")
+        self.start_time_label.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
 
-        self.verticalLayout_2.addWidget(self.label)
+        self.verticalLayout_2.addWidget(self.start_time_label)
+
+        self.end_time_label_2 = QLabel(self.layoutWidget1)
+        self.end_time_label_2.setObjectName(u"end_time_label_2")
+        self.end_time_label_2.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
+        self.verticalLayout_2.addWidget(self.end_time_label_2)
 
 
         self.retranslateUi(KokoroJourney)
@@ -83,8 +94,10 @@ class Ui_KokoroJourney(object):
         self.proc_name_show.setText(QCoreApplication.translate("KokoroJourney", u"N/A", None))
         self.proc_path_show.setText(QCoreApplication.translate("KokoroJourney", u"N/A", None))
         self.proc_start_time_show.setText(QCoreApplication.translate("KokoroJourney", u"N/A", None))
+        self.proc_end_time_show.setText(QCoreApplication.translate("KokoroJourney", u"N/A", None))
         self.proc_name_label.setText(QCoreApplication.translate("KokoroJourney", u"\u6e38\u620f\u540d\uff1a", None))
         self.proc_path_label.setText(QCoreApplication.translate("KokoroJourney", u"\u8def\u5f84\uff1a", None))
-        self.label.setText(QCoreApplication.translate("KokoroJourney", u"\u542f\u52a8\u65f6\u95f4\uff1a", None))
+        self.start_time_label.setText(QCoreApplication.translate("KokoroJourney", u"\u542f\u52a8\u65f6\u95f4\uff1a", None))
+        self.end_time_label_2.setText(QCoreApplication.translate("KokoroJourney", u"\u7ed3\u675f\u65f6\u95f4\uff1a", None))
     # retranslateUi
 
--- main.py
+++ main.py
@@ -9,49 +9,111 @@ from Ui_PidSelect import Ui_KokoroJourney
 from Ui_ProcListDialog import Ui_ProcList
 
 
+class ProcessMonitorWorker(QObject):
+    process_terminated = Signal(int, str)
+    error_occurred = Signal(str)
+    finished = Signal()
+    def __init__(self, pid_to_watch):
+        super().__init__()
+        self._pid = pid_to_watch
+        self._is_running = True
+    def run_check(self):
+        try:
+            if not psutil.pid_exists(self._pid):
+                self.error_occurred.emit(f"进程 {self._pid} 在监视开始前就不存在。")
+                return
+            while self._is_running and psutil.pid_exists(self._pid):
+                time.sleep(1)
+            if self._is_running:
+                death_time_str = datetime.datetime.now()
+                year = death_time_str.year
+                month = death_time_str.month
+                day = death_time_str.day
+                time_str = death_time_str.strftime("%H:%M:%S")
+                end_time_str = f"{year}年{month:02d}月{day:02d}日 {time_str}"
+                self.process_terminated.emit(self._pid, end_time_str)
+        except Exception as e:
+            self.error_occurred.emit(f"监视 PID {self._pid} 时发生意外错误: {e}")
+    def stop(self):
+        self._is_running = False
 
-class Mywindow(QMainWindow,Ui_KokoroJourney): #主窗口
+class Mywindow(QMainWindow,Ui_KokoroJourney):
     def __init__(self):
         super().__init__()  
         self.setupUi(self)
+        self.proc_pid = None
+        self.monitor_thread = None
+        self.worker = None
+        
         self.pushButton_procs.clicked.connect(self.open_process_dialog)
         self.proc_name_show.setText("尚未选择进程") 
         self.proc_path_show.setText("尚未选择进程") 
         self.proc_start_time_show.setText("尚未选择进程")
-    def open_process_dialog(self):  # 打开选择窗口并获取进程信息
-        dialog = DialogWindow(self) 
-        result = dialog.exec()
-        if result == QDialog.Accepted:
-            self.proc_pid = dialog.proc_pid
-            self.proc_name = dialog.proc_name
-            self.proc_path = dialog.proc_path
-            # self.pid_show.setText(self.selected_pid)
-            self.proc_name_show.setText(self.proc_name)
-            self.proc_path_show.setText(self.proc_path)
-            create_time_obj = self.get_create_timestamp(self.proc_pid)
-
-            '''
-            self.create_time_str = create_time_obj.strftime("%Y年-%m月-%d日 %H:%M:%S")  
-            不中勒哥，不支持utf8，去掉年月日是可以跑的
-            '''
-            year = create_time_obj.year
-            month = create_time_obj.month
-            day = create_time_obj.day
-            time_str = create_time_obj.strftime("%H:%M:%S")
-            self.create_time_str = f"{year}年{month:02d}月{day:02d}日 {time_str}"
+        self.proc_end_time_show.setText("尚未选择进程")
+    def open_process_dialog(self):
+        dialog = DialogWindow(self)
+        if dialog.exec() == QDialog.Accepted:
+            selected_pid = dialog.get_selected_pid()
+            if selected_pid:
+                self.update_proc_info(selected_pid)
+                self.start_monitoring_process(selected_pid)
+    def update_proc_info(self, pid):
+        try:
+            proc = psutil.Process(pid)
+            self.proc_pid = pid
+            self.proc_name_show.setText(proc.name())
+            self.proc_path_show.setText(proc.exe())
             
+            create_time = datetime.datetime.fromtimestamp(proc.create_time())
+            year = create_time.year
+            month = create_time.month
+            day = create_time.day
+            time_str = create_time.strftime("%H:%M:%S")
+            self.create_time_str = f"{year}年{month:02d}月{day:02d}日 {time_str}"
             self.proc_start_time_show.setText(self.create_time_str)
-        else:
-            print("用户取消了选择。") 
+            self.proc_end_time_show.setText("监视中...") 
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            self.proc_name_show.setText(f"无法访问进程 {pid}")
+            self.proc_path_show.setText("N/A")
+            self.proc_start_time_show.setText("N/A")
+            self.proc_end_time_show.setText("N/A")
+    def start_monitoring_process(self, pid):
+        if self.monitor_thread and self.monitor_thread.isRunning():
+            self.worker.stop()
+            self.monitor_thread.quit()
+            self.monitor_thread.wait()
+        self.monitor_thread = QThread()
+        self.worker = ProcessMonitorWorker(pid)
+        self.worker.moveToThread(self.monitor_thread)
+        self.monitor_thread.started.connect(self.worker.run_check)
+        self.worker.process_terminated.connect(self.on_process_terminated)
+        self.worker.error_occurred.connect(self.on_monitor_error)
+        self.monitor_thread.finished.connect(self.on_monitor_finished)
+        self.monitor_thread.finished.connect(self.worker.deleteLater)
+        self.monitor_thread.start()
+    def on_process_terminated(self, pid, end_time_str):
+        if self.proc_pid != pid:
+            print(f"[Manager] 这是一个过时的报告，忽略。")
+            return
+        self.proc_end_time_show.setText(end_time_str)
+    def on_monitor_finished(self):
+        print("[Manager] 确认监视线程已结束。")
+    def on_monitor_error(self, error_message):
+        print(f"[Manager] 接到错误报告: {error_message}")
+    def closeEvent(self, event):
+        if self.monitor_thread and self.monitor_thread.isRunning():
+            self.worker.stop()
+            self.monitor_thread.quit()
+            self.monitor_thread.wait()
+        super().closeEvent(event)
     def get_create_timestamp(self,pid):
         proc = psutil.Process(pid)
         create_timestamp = proc.create_time()
         create_datetime = datetime.datetime.fromtimestamp(create_timestamp)
         return create_datetime
 
-
 class DialogWindow(QDialog,Ui_ProcList):
-    def __init__(self, parent=None): #进程选择窗口
+    def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
         self.proc_pid = None
@@ -65,10 +127,16 @@ class DialogWindow(QDialog,Ui_ProcList):
         self.procTable.setSelectionBehavior(QAbstractItemView.SelectRows)  # 行选择
         self.procTable.setSelectionMode(QAbstractItemView.SingleSelection)  # 选择单位数量
         self.procTable.setEditTriggers(QAbstractItemView.NoEditTriggers)  # 不可编辑
-        self.pushButton_accept.clicked.connect(self.get_proc_info_and_aceept)
+        self.pushButton_accept.clicked.connect(self.accept) # 直接接受
         self.pushButton_reject.clicked.connect(self.reject)
         self.populate_process_list()
-    def populate_process_list(self): #进程列表填入
+    def get_selected_pid(self): 
+        selected_rows_indexes = self.procTable.selectionModel().selectedRows()
+        if not selected_rows_indexes:
+            return None
+        row = selected_rows_indexes[0].row()
+        return self.procTable.item(row, 0).data(Qt.ItemDataRole.UserRole)
+    def populate_process_list(self):
         processes = get_process_list()
         self.procTable.setRowCount(len(processes))
         for row, proc_info in enumerate(processes):
@@ -84,26 +152,9 @@ class DialogWindow(QDialog,Ui_ProcList):
             self.procTable.setItem(row, 0, pid_item)
             self.procTable.setItem(row, 1, name_item)
             self.procTable.setItem(row, 2, path_item)
-    def get_proc_info_and_aceept(self): #获取进程信息并接受
-        selected_rows_indexes = self.procTable.selectionModel().selectedRows()
-        if not selected_rows_indexes:
-            print("没有选中的行。")
-            return None, None
-        first_selected_row_index = selected_rows_indexes[0]
-        row = first_selected_row_index.row()
-        pid = self.procTable.item(row, 0).data(Qt.ItemDataRole.UserRole)
-        name = self.procTable.item(row, 1).text()
-        path = self.procTable.item(row, 2).text()
-        self.proc_pid = pid
-        self.proc_name = name
-        self.proc_path = path
-        print(f"对话框内部已存储 -> PID: {self.proc_pid},\
-                进程名: {self.proc_name}, \
-                路径: {self.proc_path}")
-        self.accept()
     
 
-def get_process_list(): #进程获取
+def get_process_list():
     attrs = ['pid', 'name', 'exe']
     process_data = []
     path_separator = os.sep
